f4078e43614c90a9130de2101a82c583509c6370
andrew <cieslik.andrew@gmail.com>
2024-07-16 09:48:08 +0200

    Program 1.2

--- Program_1_1/src/Main.java
+++ Program_1_1/src/Main.java
@@ -2,7 +2,6 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         String message = JOptionPane.showInputDialog(null, "Enter your message:", "Input Dialog", JOptionPane.QUESTION_MESSAGE);
         if (message != null) {
             JOptionPane.showMessageDialog(null, message.toUpperCase(), "Message Dialog", JOptionPane.INFORMATION_MESSAGE);
--- Program_1_2/src/Main.java
+++ Program_1_2/src/Main.java
@@ -1,5 +1,22 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class Main {
+
+    public static BigInteger calculateFactorial(int n) {
+        BigInteger result = BigInteger.ONE;
+        for (int i = 1; i <= n; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a non-negative integer: ");
+        int n = scanner.nextInt();
+
+        BigInteger factorial = calculateFactorial(n);
+        System.out.println("Factorial of " + n + " is: " + factorial);
     }
 }
