77c2d70903fb1abf37f169bb1ffee929f8590df5
andrew <cieslik.andrew@gmail.com>
2024-07-16 09:24:18 +0200

    Program 1.1

--- Program_1_1/src/Main.java
+++ Program_1_1/src/Main.java
@@ -1,5 +1,15 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world1_1!");
+        System.out.println("Hello world!");
+        String message = JOptionPane.showInputDialog(null, "Enter your message:", "Input Dialog", JOptionPane.QUESTION_MESSAGE);
+        if (message != null) {
+            JOptionPane.showMessageDialog(null, message.toUpperCase(), "Message Dialog", JOptionPane.INFORMATION_MESSAGE);
+            System.exit(0);
+        } else {
+            JOptionPane.showMessageDialog(null, "No message entered!", "Message Dialog", JOptionPane.WARNING_MESSAGE);
+            System.exit(1);
+        }
     }
 }
